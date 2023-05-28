3205ace58b57bfe41023b76cd65d3de80e00414c
hhuynh <hhuynh.dev@gmail.com>
2023-05-27 18:06:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
 import os
-
+from flask import Flask, render_template, request, session
 import openai
-from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+app.secret_key = 'e5ac358c-f0bf-11e5-9e39-d3b532c10a28'  # Use a secure, unique, and secret value here
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+@app.route("/", methods=["GET", "POST"])
+def chat():
+    if 'messages' not in session:
+        session['messages'] = [{'role':'system', 'content':'You are a helpful assistant.'}]
 
-@app.route("/", methods=("GET", "POST"))
-def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        user_message = request.form["message"]
+        session['messages'].append({'role':'user', 'content':user_message})
+
+        # Create a chat message for the API
+        chat_message = [{'role':msg['role'], 'content':msg['content']} for msg in session['messages']]
+
+        response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=chat_message
         )
-        return redirect(url_for("index", result=response.choices[0].text))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        assistant_message = response['choices'][0]['message']['content']
+        session['messages'].append({'role':'assistant', 'content':assistant_message})
+
+    return render_template("index.html", chat=session['messages'])
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+if __name__ == "__main__":
+    app.run(debug=True)
